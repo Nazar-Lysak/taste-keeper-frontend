6324d8d1880b6e0afbf53a0034151d2394b06a42
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2025-10-19 10:35:03 +0300

    fix code

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
-import { useTranslation } from "react-i18next";
 import Home from "./page/home-page/Home";
 
 function App() {
-  const { t } = useTranslation("common");
 
   return (
     <div style={{ overflow: "hidden" }}>
--- src/client/forms/auth-form/AuthForm.tsx
+++ src/client/forms/auth-form/AuthForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { motion, AnimatePresence, m } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "./AuthForm.module.scss";
 import { useTranslation } from "react-i18next";
 
